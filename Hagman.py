3a288645ccbf843388091c3949fdacc87739e457
Anevereto <jinjanwarrior@gmail.com>
2022-08-20 15:19:26 +0100

    Hagman.py

--- Hagman.py
+++ Hagman.py
@@ -1,39 +1,52 @@
-#from words import words
+from words import words
 import random
 import string
 
-words = ["aback","abaft","abandoned","abashed","aberrant","abhorrent","abiding","abject","ablaze","able","abnormal","aboard","aboriginal","abortive","abounding","abrasive","abrupt","absent","absorbed","absorbing"]
-
-def valid_word(words):
+def valid_word(words): # se encarga de elejir de la lista una palabra válida, ignorando comas, espacios o guiones. Pra luego devolver la palabra válida.
     word  = random.choice(words)
-    while '-' in word and ' ':
+    while '-' in word or ' ' in word:
         word  = random.choice(words)
     
-    return word
+    return word.upper()
 
 def hangman():
     word = valid_word(words)
-    word_letters = set(word)
-    alphabet = set(string.ascii_uppercase)
-    used_letters = set()
+    word_letters = set(word) # un set de todas la letras de la palabra
+    alphabet = set(string.ascii_uppercase) # un set de todas la lestras del alfabeto
+    used_letters = set() 
 
-    while len(word_letters) > 0:
-        print("Yo have use this letters: ", ' '.join(used_letters))
+    lives = 6
 
-        word_list = [letter if letter in used_letters else '_' for letter in word]
-        print("Current word: ", ' '.join(word_list), word)
+    while len(word_letters) > 0 and lives > 0:
+        print("You have", lives, "lives left" "\n" "Yo have use this letters: ", ' '.join(used_letters)) # va añadiendo las letras que ha ido usando el usuario
+
+        word_list = [letter if letter in used_letters else '_' for letter in word] # almacena las letras adivinadas de la palabra
+        print("Current word: ", ' '.join(word_list)) # escribe las letras adivinadas de la palabra
 
         user_letter = input("Gues a letter: ").upper()
-        if user_letter in alphabet - used_letters:
+        if user_letter in alphabet - used_letters: 
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else: 
+                lives = lives -1
+                print("This letter is not in the word")
         
         elif user_letter in used_letters:
             print("You have alredy use this letter")
         
         else:
             print("Invalid character")
+
+    if lives == 0:
+        print("You died, the word was", word)
+    else:
+        print("You guessed the word", word, "!!!")
+    replay = str(input("Do you want to play againg? (yes/no): "))
+    if replay == "yes":
+        hangman()
+    else:
+        print("Bye bye!!, see you soon!")
 
 hangman()
 
